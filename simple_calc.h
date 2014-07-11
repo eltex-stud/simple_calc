a94eb880a97b933624076f5ccf5dbeb992e7a5f5
Stephan Schauer <trob_exe@mail.ru>
2014-07-11 14:38:47 +0700

    added ifndef for header

--- simple_calc.h
+++ simple_calc.h
@@ -1,3 +1,5 @@
+#ifndef SIMPLE_CALC_H_
+#define SIMPLE_CALC_H_
 #include <stdio.h>
 #include <math.h>
 
@@ -6,3 +8,5 @@ void func_list();
 float divide(float arg1, float arg2);
 float sum(float arg1, float arg2);
 float sum_then_divide(float arg1, float arg2, float arg3);
+
+#endif // SIMPLE_CALC_H_
