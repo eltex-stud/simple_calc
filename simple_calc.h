228983a7bc61d1b8ac82052af4108e03b69406a5
Vadim <na>
2014-07-11 15:56:25 +0700

    Changed simple_calc.h

--- simple_calc.h
+++ simple_calc.h
@@ -9,6 +9,6 @@ float sum(float arg1, float arg2);
 float multiple(float arg1, float arg2);
 float sum_then_divide(float arg1, float arg2, float arg3);
 float sub(float arg1, float arg2);
-float sub_then_mult(float arg1, float arg2, float arg3);
+float sum_then_mult(float arg1, float arg2, float arg3);
 
 #endif // SIMPLE_CALC_H_
